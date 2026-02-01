5f08f7e0285552c47ffb32aafe4ca8a09267669b
ankyadav82 <ankityadav1619@gmail.com>
2026-02-01 17:52:08 +0530

    Add java file

---
+++ AddTwoNumber.java
@@ -0,0 +1,9 @@
+public class AddTwoNumber {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a+b;
+        System.out.println("The sum of " +a + " and " + b+ " is: " + sum);
+    }
+    
+}
---
+++ SubtractTwoNumber.java
@@ -0,0 +1,9 @@
+public class SubtractTwoNumber {
+    public static void main(String[] args){
+        int a=10;
+        int b=5;
+        int sub=a-b;
+        System.out.print("The subtraction of " + a + " And " + b +" is: " + sub);
+    }
+    
+}
