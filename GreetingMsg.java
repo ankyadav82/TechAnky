0ea305d1671aed71b62ff4d31f4216c6723b2a55
ankyadav82 <ankityadav1619@gmail.com>
2026-02-01 18:36:18 +0530

    java program

---
+++ GreetingMsg.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class GreetingMsg {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name=sc.nextLine();
+        System.out.println("Hello " +name + " Welcome to java programming!");
+    }
+    
+}
