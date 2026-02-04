dded39deb0df0d19ded5545d96a3ff82627b812f
ankyadav82 <ankityadav1619@gmail.com>
2026-02-05 00:00:26 +0530

    loop

---
+++ Loop.java
@@ -0,0 +1,17 @@
+
+public class Loop {
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=i;j++){
+                if(j%2==0)
+                    System.out.print(" ");
+                else
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
